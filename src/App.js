bef549a50b1ed601f9cf8e78e75d91b8e95077ba
Andrew Kroll <andrew.kroll@yahoo.com>
2018-09-12 12:26:40 -0400

    basic getAllBooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import {GetBooks,CreateBook} from './Books'
+import {GetBooks} from './Books'
 
 const App = () => (
         <div>
             <h1>My first Apollo app 🚀</h1>
             <GetBooks/>
-            <CreateBook/>
         </div>
 );
 
--- src/Books/Books.js
+++ src/Books/Books.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import './books.css'
-import { Query, Mutation } from "react-apollo";
+import { Query } from "react-apollo";
 import gql from "graphql-tag";
-var uuid = require('uuid4');
 
 const GET_BOOKS = gql`
     {
         books {
             title
             author
-            reviews {
-                review
-            }
         }
     }
 `;
@@ -26,13 +22,10 @@ export const GetBooks = () => (
                 if (loading) return <p>Loading...</p>;
                 if (error) return <p>Error :(</p>;
 
-                return data.books.map(({ title, author, reviews }) => (
+                return data.books.map(({ title, author }) => (
                     <div key={title}>
                         <p>{title}</p>
                         <p>{author}</p>
-                        {/*{reviews.map(r => (*/}
-                            {/*<p>{r.review}</p>)*/}
-                        {/*)}*/}
                     </div>
                 ))
             }}
@@ -40,61 +33,3 @@ export const GetBooks = () => (
 
     </div>
 );
-
-const CREATE_BOOK = gql`
-    mutation CreateBook($input: BookInput) {
-        createBook(input: $input){
-            title
-            _id
-        }
-    }
-`;
-
-export const CreateBook = () => {
-    let input = {};
-
-    return (
-        <div>
-            <h1>Create A Book Record</h1>
-            <Mutation mutation={CREATE_BOOK}>
-                {(createBook, {data}) => {
-                    console.log('data: ',data);
-                    return (
-                    <div>
-                        <form
-                            onSubmit={e => {
-                                e.preventDefault();
-                                createBook({
-                                    variables: {
-                                        input: {
-                                            uuid: uuid(),
-                                            title: input.title.value,
-                                            author: input.author.value
-                                        }
-                                    }
-                                });
-                                input.title.value = "";
-                                input.author.value = "";
-                            }}
-                        >
-                            <label htmlFor="title">Title</label>
-                            <input
-                                ref={title => {
-                                    input.title = title;
-                                }}
-                            />
-                            <label htmlFor="author">Author</label>
-                            <input
-                                ref={author => {
-                                    input.author = author;
-                                }}
-                            />
-                            <button type="submit">Create Book</button>
-                        </form>
-                    </div>
-                    )}}
-
-            </Mutation>
-        </div>
-    );
-};
